523556fb1b089c007f1c3afca4813c964d470125
genga1 <anthony.geng@pery.com>
2019-01-18 17:52:51 +0800

    init project

--- WorkInstruction/App_Start/WebApiConfig.cs
+++ WorkInstruction/App_Start/WebApiConfig.cs
@@ -14,9 +14,10 @@ namespace WorkInstruction
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
